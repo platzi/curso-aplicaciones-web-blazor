06f4904c30d6cb07e39b6741a77f10939298f639
Miguel Teheran <miguelteheran@gmail.com>
2022-09-01 14:50:27 -0500

    configurando conexion a api

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using blazorappdemo;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
 await builder.Build().RunAsync();
